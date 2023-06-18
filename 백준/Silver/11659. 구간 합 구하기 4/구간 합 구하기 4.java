a0c1b39bab465831c6e8e18589ae2fca6b6b948b
Dibi <134939365+DevSeungHyeon@users.noreply.github.com>
2023-06-18 20:25:10 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 656 ms, Memory: 53888 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 기본 정보 입력
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 숫자 정보 입력 및 누적합 생성
+        int[] number = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        
+        number[1] = Integer.parseInt(st.nextToken());
+
+        for (int i = 2; i <= N; i++) {
+            number[i] = number[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        // M번의 구간합 구하기
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+        
+            int result = number[end] - number[start-1];
+            sb.append(result).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
