deaff4876050ac6869aac3dd2ac24ba881024de2
DevSeungHyeon <134939365+devdibi@users.noreply.github.com>
2024-01-09 20:51:45 +0900

    [level unrated] Title: 자릿수 더하기, Time: 0.11 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,10 +1,13 @@
 class Solution {
     public int solution(int n) {
-        String[] number = (n + "").split("");
+        String[] number = Integer.toString(n).split("");
+        
         int answer = 0;
+        
         for(String s : number){
              answer += Integer.parseInt(s);
         }
+        
         return answer;
     }
 }
