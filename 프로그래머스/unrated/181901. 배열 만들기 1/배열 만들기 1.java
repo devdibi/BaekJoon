7a5983c12f5d6a910b9d44e6d01d95fbb58ddd82
DevSeungHyeon <134939365+devdibi@users.noreply.github.com>
2024-01-10 14:54:51 +0900

    [level unrated] Title: 배열 만들기 1, Time: 10.53 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        
+        return IntStream.rangeClosed(1,n)
+                .filter(a -> a % k == 0)
+                .toArray();
+    }
+}
