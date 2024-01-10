9bdd98d9aea0ae9df8daf91c24de7db762b5290c
DevSeungHyeon <134939365+devdibi@users.noreply.github.com>
2024-01-10 13:47:01 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        if(my_string.endsWith(is_suffix)){
+            return 1;
+        }
+        return 0;
+    }
+}
