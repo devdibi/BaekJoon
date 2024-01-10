47ea71fce3bf6141c0cdbc80a59a31930e9d027d
DevSeungHyeon <134939365+devdibi@users.noreply.github.com>
2024-01-11 00:49:57 +0900

    [level unrated] Title: 개미 군단, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int hp) {
+        int cnt = 0;
+        if(hp >= 5){
+            cnt += hp/5;
+            hp %= 5;
+        }
+        
+        if(hp >= 3){
+            cnt += hp/3;
+            hp %= 3;
+        }
+        
+        cnt += hp;
+        return cnt;
+    }
+}
