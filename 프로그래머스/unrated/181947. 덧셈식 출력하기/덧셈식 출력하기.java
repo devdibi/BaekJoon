16327cdb60fea305ec6be43c8895c77c7b7a452b
DevSeungHyeon <134939365+devdibi@users.noreply.github.com>
2024-01-11 12:22:19 +0900

    [level unrated] Title: 덧셈식 출력하기, Time: 172.38 ms, Memory: 68.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(a)
+            .append(" ")
+            .append('+')
+            .append(" ")
+            .append(b)
+            .append(" ")
+            .append('=')
+            .append(" ")
+            .append(a+b);
+        System.out.println(sb.toString());
+    }
+}
