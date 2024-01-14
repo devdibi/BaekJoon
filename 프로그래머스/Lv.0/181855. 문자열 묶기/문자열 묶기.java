7b7efcd761b6b67fe5d0ffdeb579ca8cae3c3739
DevSeungHyeon <134939365+devdibi@users.noreply.github.com>
2024-01-14 23:44:29 +0900

    [level Lv.0] Title: 문자열 묶기, Time: 5.39 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/Lv.0/181855. 문자열 묶기/문자열 묶기.java
@@ -1,18 +1,12 @@
 class Solution {
     public int solution(String[] strArr) {
-        int max = -1;
-        
-        for(String str : strArr){
-            max = Math.max(max, str.length());
-        }
-        
-        int[] cnt = new int[max+1];
+        int[] cnt = new int[31];
         
         for(String s : strArr){
             cnt[s.length()]++;
         }
         
-        max = -1;
+        int max = -1;
         
         for(int c : cnt){
             max = Math.max(max, c);
