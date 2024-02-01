855a2c7767471257c1d84c9a02744032a71d583d
DevSeungHyeon <134939365+devdibi@users.noreply.github.com>
2024-02-01 09:57:24 +0900

    [level 2] Title: 피로도, Time: 0.13 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,21 @@
 /*
     풀이
-        모든 경우를 탐색하고 가장 많은 던전을 탐험할 수 있는 경우의 수를 구한다.
+        모든 경우를 탐색하고 가장 많은 던전을 탐험할 수 있는 경우의 수를 구한다.(n <= 8)
         
-
 */
 
 import java.util.*;
 
 class Solution {
-    int[][] d;
-    int t;
-    int[] select;
-    boolean[] visited;
-    int answer = 0;
+    int[][] d; // dungeons
+    int t; // k
+    int[] select; // 선택 배열
+    boolean[] visited; // 방문 배열
+    int answer = 0; // 정답
+    
     public int solution(int k, int[][] dungeons) {
-        // 전역변수화
         d = dungeons;
         t = k;
-        
         select = new int[dungeons.length];
         visited = new boolean[dungeons.length];
         
@@ -27,11 +25,11 @@ class Solution {
     
     public void dfs(int cnt){
         if(cnt == d.length){
-            int result = 0;
-            int total = t;
+            int result = 0; // 수행 가능한 던전 수
+            int total = t; // 전체 피로도
             
             for(int i : select){
-                if(total < d[i][0]) continue;
+                if(total < d[i][0]) continue; // 수행할 수 없는 던전 패스
                 
                 total -= d[i][1];
                 result++;
