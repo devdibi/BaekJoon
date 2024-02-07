0f8c75e3402a5bb03a7096d1f78c6fbde5de5eba
DevSeungHyeon <134939365+devdibi@users.noreply.github.com>
2024-02-07 13:51:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.49 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -6,9 +6,10 @@
 import java.util.*;
 
 class Solution {
+    // 위치 저장 클래스
     public class Loc{
-        int r, c;
-        int d;
+        int r, c; // 행, 렬
+        int d; // 이동 거리
         
         public Loc(int r, int c, int d){
             this.r = r;
@@ -16,23 +17,24 @@ class Solution {
             this.d = d;
         }
     }
+    // BFS를 위한 큐
     Queue<Loc> queue = new LinkedList<>();
     
-    int[] r = {-1,0,0,1};
+    // 상, 하, 좌, 우
+    int[] r = {-1,0,0,1}; 
     int[] c = {0,-1,1,0};
     
-    int[][] map;
-    boolean[][] visited;
     
-    int n, m;
+    int[][] map; // 지도 정보
+    boolean[][] visited; // 방문배열
+    int n, m; // 지도 크기
     public int solution(int[][] maps) {
         map = maps;
-        
         n = map.length;
         m = map[0].length;
-        
         visited = new boolean[n][m];
         
+        // 이동하지 못하는 위치 표시
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(map[i][j] != 1){
@@ -41,28 +43,34 @@ class Solution {
             }
         }
         
-        int result = move().d;
+        // BFS 수행
+        int result = bfs().d;
         
         return result;
     }
-    public Loc move(){
+    public Loc bfs(){
+        // 최초 시작 위치
         queue.offer(new Loc(0,0,1));
         visited[0][0] = true;
         
         while(!queue.isEmpty()){
-            Loc temp = queue.poll();
+            Loc temp = queue.poll(); // 이동 전 위치
             for(int i = 0; i < 4; i++){
+                // 이동 후 위치
                 int nr = temp.r + r[i];
                 int nc = temp.c + c[i];
                 
+                // 범위 밖
                 if(nr < 0 || nc < 0 || nr >= n || nc >= m) continue;
                 
+                // 방문 여부 확인
                 if(visited[nr][nc]) continue;
                 
+                // 종료 여부 확인
                 if(nr == n-1 && nc == m-1) return new Loc(nr,nc,temp.d+1);
                 
-                visited[nr][nc] = true;
-                queue.offer(new Loc(nr,nc,temp.d + 1));
+                visited[nr][nc] = true; // 방문 체크
+                queue.offer(new Loc(nr,nc,temp.d + 1)); // 다음 위치 등록
             }
         }
         return new Loc(0,0,-1);
