9d41c5542c31cd70d2cc5bdf46336b8f4a4e43db
DevSeungHyeon <134939365+devdibi@users.noreply.github.com>
2024-04-17 00:55:48 +0900

    [level 2] Title: 땅따먹기, Time: 15.53 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,9 +1,9 @@
 class Solution {
     int solution(int[][] land) {
-        int[][] dp = new int[land.length][4];
+        int[][] dp = new int[land.length][4]; // DP 저장 배열
         
         for(int i  = 0; i < 4; i++){
-            dp[0][i] = land[0][i];
+            dp[0][i] = land[0][i]; // 맨 처음 행 저장
         }
         
         // 행
@@ -12,7 +12,9 @@ class Solution {
             for(int j = 0; j < 4; j++){
                 // land 열
                 for(int k = 0; k < 4; k++){
-                    if(k == j) continue;
+                    if(k == j) continue; // 같은 열 선택 불가
+                    
+                    // 해당 열을 선택했을 경우 가장 큰 값을 갖는 경우 계산
                     if(dp[i][j] < land[i][j] + dp[i-1][k]){
                         dp[i][j] = land[i][j] + dp[i-1][k];
                     }
@@ -22,7 +24,7 @@ class Solution {
         
         int answer = -1;
         for(int i = 0; i < 4; i++){
-            answer = Math.max(answer, dp[land.length-1][i]);
+            answer = Math.max(answer, dp[land.length-1][i]); // 모두 더한 값에서 최대값 판별
         }
         
         return answer;
